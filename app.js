a6ff230bbf2d9e85d9601982bb49cb9cda2a47a0
Sure Rocha <surerochab@gmail.com>
2024-08-06 16:38:59 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+//API Rest - Disciplina: Programação Web
+//Autor: Sure Rocha
+
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port, ()=> {
+  console.log('Hello API on port 8000');
+})
+
+//Rota raiz
+app.get('/', function(req, res) {
+res.send("Coloque os devidos endpoints na URL. -> Opções: /v1/:name ou /v2/:name ou /v3/pt-br/:name ou /v3/en-us/:name ou /v3/es-ar/:name");
+})
+
+//V1
+app.get('/v1/:name', function(req, res) {
+res.json({msg: "hello v1, "+ req.params.name});
+})
+
+//V2
+app.get('/v2/:name', function(req, res) {
+res.json({msg: "hello v2, "+ req.params.name});
+})
+
+//V3
+app.get('/v3/pt-br/:name', function(req, res) {
+res.json({msg: "Olá, querido "+ req.params.name});
+})
+
+app.get('/v3/en-us/:name', function(req, res) {
+res.json({msg: "Hello, dear "+ req.params.name});
+})
+
+app.get('/v3/es-ar/:name', function(req, res) {
+res.json({msg: "Hola, querido "+ req.params.name});
+})
+
+//Another option
+// app.get('/v3/:lang/:name', function(req, res) {
+//   if (req.params.lang === 'pt-br') {
+//     res.json({msg: "Olá, querido "+ req.params.name});
+//   } else if (req.params.lang === 'en-us') {
+//     res.json({msg: "Hello, dear "+ req.params.name});
+//   } else if (req.params.lang === 'es-ar') {
+//     res.json({msg: "Hola, querido "+ req.params.name});
+//   } else {
+//     res.status(404).json({msg: 'Endpoint not found'});
+//   }
+// });
+
+//CATCH ERROR 404
+app.use((req, res, next) => {
+  res.status(404).json({msg: 'Endpoint not found.'});
+});
